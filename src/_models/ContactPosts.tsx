b360e3f9aec3e085c5a7f62adbef60290893b77a
m4thieulavoie <mathieulavoie94@gmail.com>
2020-04-16 18:55:33 -0400

    feat: added bmc button

--- src/_models/ContactPosts.tsx
+++ src/_models/ContactPosts.tsx
@@ -5,11 +5,31 @@ import Emoji from "../emoji/Emoji";
 export const contactPosts = [
   {
     text: (
+      <>
+        <h3>Like what I'm doing?</h3>
+        <p>I love coffee!</p>
+        <a
+          className="bmc-button"
+          target="_blank"
+          href="https://www.buymeacoffee.com/mathieulavoie"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+            alt="Buy me a coffee!"
+          />
+          <span>Buy me one!</span>
+        </a>
+      </>
+    ),
+  },
+  {
+    text: (
       <p className="status">
         <Emoji emoji="✉️"></Emoji>{" "}
         <a href="mailto:mathieulavoie94@gmail.com">mathieulavoie94@gmail.com</a>
       </p>
-    )
+    ),
   },
   {
     date: "2015 - now",
@@ -20,7 +40,7 @@ export const contactPosts = [
             <a href="https://codepen.io/matmobile">Codepen</a>
           </li>
           <li>
-            <a href="https://github.com/matmobile">GitHub</a>
+            <a href="https://github.com/m4thieulavoie">GitHub</a>
           </li>
           <li>
             <a href="https://www.linkedin.com/in/mathieu-lavoie-4a2a99122/">
@@ -35,6 +55,6 @@ export const contactPosts = [
           </li>
         </ul>
       </div>
-    )
-  }
+    ),
+  },
 ] as SocialPost[];
