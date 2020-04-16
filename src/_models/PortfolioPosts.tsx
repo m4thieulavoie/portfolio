a24a330b4b80f776cde08087265e3a4041b7b1a4
m4thieulavoie <mathieulavoie94@gmail.com>
2020-04-16 19:13:10 -0400

    feat: added s2s in portfolio

--- src/_models/PortfolioPosts.tsx
+++ src/_models/PortfolioPosts.tsx
@@ -16,7 +16,31 @@ export const protfolioPosts = [
           alt="GoToConnect"
         />
       </>
-    )
+    ),
+  },
+  {
+    date: "April 2020",
+    text: (
+      <>
+        <h2>sass-to-string</h2>
+        <p>
+          Nifty webpack loader that converts a <code>.scss</code> file into a
+          javascript string.{" "}
+          <a
+            href="https://www.npmjs.com/package/sass-to-string"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            See it on npmjs!
+          </a>
+        </p>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg"
+          alt="SASS Logo"
+          width="200"
+        />
+      </>
+    ),
   },
   {
     date: "2019",
@@ -26,7 +50,7 @@ export const protfolioPosts = [
         <p>Did a Front-End refresh for the whole Magazine</p>
         <img src="/images/mca.png" alt="Magazine Web Motoneiges.ca" />
       </>
-    )
+    ),
   },
   {
     date: "2018",
@@ -36,7 +60,7 @@ export const protfolioPosts = [
         <p>Did a Front-End app for the vehicule dealer</p>
         <img src="/images/yvanthibault.png" alt="Garage Yvan Thibault" />
       </>
-    )
+    ),
   },
   {
     date: "2018",
@@ -46,6 +70,6 @@ export const protfolioPosts = [
         <p>Did a Front-End app for the vehicule dealer</p>
         <img src="/images/plabonte.png" alt="P. Labonté & Fils" />
       </>
-    )
-  }
+    ),
+  },
 ] as SocialPost[];
