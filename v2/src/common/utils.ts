721a01283f48946d3eefeec2201f04c2e7124b93
Matt Lavoie <mathieulavoie94@gmail.com>
2022-06-14 13:08:12 -0400

    wip

--- v2/src/common/utils.ts
+++
@@ -1,7 +0,0 @@
-export const navigateToPage = (e: MouseEvent | undefined, page: string) => {
-  if (e?.preventDefault) {
-    e.preventDefault();
-  }
-  window.location.href = page;
-  window.scrollTo(0, 0);
-};
